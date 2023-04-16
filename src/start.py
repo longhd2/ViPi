e5c2a5409f63a3a174125b6df8e26e350d6fdb58
longhd2 <43842525+longhd2@users.noreply.github.com>
2023-04-16 22:47:06 +0700

    Update start.py

--- src/start.py
+++ src/start.py
@@ -1,29 +1,10 @@
 import platform, os, json, time
-from actions import USER_PATH,configuration
+from actions import USER_PATH,say
 
-# try:      
-    # with open('{}/.config/google-oauthlib-tool/credentials.json'.format(USER_PATH), 'r') as registers:
-        # register = json.load(registers)
-    # register = False
-# except:
-    # register=(True if configuration["Use_Google_Assistant"].upper() == "OK" else False)
-# if register:
-    # print('Bạn chưa đăng ký tài khoản với google, thực hiện đăng ký tài khoản')
-    # from new_oauth import *
-    # if __name__ == '__main__':
-       # app.run(host='0.0.0.0',port=5002)
-    # pass
-# else:
-if configuration['Start_config']['Startup_file']=='new_main':
+try:      
     import new_main
     print ('\nimport new_main')
     new_main.google_sdk().main()
-if configuration['Start_config']['Startup_file']=='new_start':
-    import new_start
-    print ('\nimport new_start')
-    new_start.main()
-# else:
-        # from actions import say_save
-        # say_save('đã xảy ra lỗi,vui lòng kiểm tra cài đặt và khởi động lại')
-        # time.sleep(5)
-        # pass
+except:
+    say('gặp lỗi khi khởi động vui lòng kiểm tra lại cài đặt')
+
