f57af310b459d02d67849115dd34b5fc3845c1fc
longhd2 <43842525+longhd2@users.noreply.github.com>
2023-09-05 16:33:47 +0700

    Update cus_skill.py

--- src/cus_skill.py
+++ src/cus_skill.py
@@ -20,59 +20,36 @@
 # Ví dụ với skill Happy__Birthday:
 #		"cus__happy__birthday": [
 #			"mừng sinh nhật",
-#			"birthday",
+#			"birthday"
+#		]
 #		"cus__happy__new__year": [
 #			"chúc tết",
-#			"tết nguyên đán",
+#			"tết nguyên đán"
+#		]
 ###########################
 
 import random
 
-def cus__skill(skill, data):
 #hàm chúc mừng sinh nhật
-    if skill == 'cus__happy__birthday':
-        answers = [
-            "Chúc mừng sinh nhật! Mong rằng bạn có một ngày thật vui vẻ và đầy ý nghĩa.",
-            "Chúc mừng sinh nhật! Hy vọng năm mới của bạn sẽ đem lại nhiều thành công và niềm vui.",
-            "Happy birthday! Chúc bạn luôn khỏe mạnh, hạnh phúc và thành công trong cuộc sống.",
-            "Sinh nhật vui vẻ! Hãy tận hưởng ngày đặc biệt này cùng gia đình và bạn bè của bạn."
-        ]
-        answer = random.choice(answers)
-        return answer, True
+def cus__happy__birthday(skill,data):
+    answers = [
+        "Chúc mừng sinh nhật! Mong rằng bạn có một ngày thật vui vẻ và đầy ý nghĩa.",
+        "Chúc mừng sinh nhật! Hy vọng năm mới của bạn sẽ đem lại nhiều thành công và niềm vui.",
+        "Happy birthday! Chúc bạn luôn khỏe mạnh, hạnh phúc và thành công trong cuộc sống.",
+        "Sinh nhật vui vẻ! Hãy tận hưởng ngày đặc biệt này cùng gia đình và bạn bè của bạn."
+    ]
+    answer = random.choice(answers)
+    return answer, True
 #hàm chúc Tết
-    if skill == 'cus__happy__new__year':
-        answers = [
-            "Chúc mừng năm mới! Mong rằng năm mới mang lại nhiều niềm vui, thành công và sức khỏe cho bạn.",
-            "Chúc mừng năm mới! Hy vọng năm mới sẽ đem lại nhiều cơ hội và thành tựu lớn cho bạn.",
-            "Happy New Year! Chúc bạn có một năm mới thật phát đạt và hạnh phúc.",
-            "Năm mới an lành! Chúc bạn và gia đình có một kỳ nghỉ vui vẻ và đầy ý nghĩa."
-        ]
-        answer = random.choice(answers)
-        return answer, True
-#hàm đọc tin tức     
-    # if skill == 'cus__get__news':
-        # url = 'https://example.com/news'  # Thay đổi URL thành trang web tin tức bạn muốn lấy
+def cus__happy__new__year(skill,data):
+    answers = [
+        "Chúc mừng năm mới! Mong rằng năm mới mang lại nhiều niềm vui, thành công và sức khỏe cho bạn.",
+        "Chúc mừng năm mới! Hy vọng năm mới sẽ đem lại nhiều cơ hội và thành tựu lớn cho bạn.",
+        "Happy New Year! Chúc bạn có một năm mới thật phát đạt và hạnh phúc.",
+        "Năm mới an lành! Chúc bạn và gia đình có một kỳ nghỉ vui vẻ và đầy ý nghĩa."
+    ]
+    answer = random.choice(answers)
+    return answer, True
 
-        # try:
-            # response = requests.get(url)
-            # response.raise_for_status()
-            # soup = BeautifulSoup(response.text, 'html.parser')
-            
-            # # Lấy tiêu đề tin tức
-            # title = soup.find('h1').text.strip()
 
-            # # Lấy nội dung tin tức
-            # content = soup.find('div', class_='content').text.strip()
-
-            # # Tạo dictionary chứa thông tin tin tức
-            # news = {
-                # 'title': title,
-                # 'content': content
-            # }
-
-            # return news, True
-        # except requests.exceptions.RequestException as e:
-            # return str(e), True
-    
-    return "Kỹ năng không được hỗ trợ", True
 
